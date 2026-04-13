4ed366cc164daa2f254bb505feb843532bd97f3c
Ponnesh18 <ponnesh18@gmail.com>
2026-04-13 09:09:06 +0530

    Data Types file commit

---
+++ DataTypes.js
@@ -0,0 +1,39 @@
+// ECMA standard 
+
+"use strict"  // treat all Js code as  newer version
+
+// code readability should be high 
+console.log(3+
+    
+    3)  // X X X X
+
+    //Data types
+
+    // Number 
+    let number = 1234567668;
+    console.log(number);
+
+    //bigint ==> big integer value
+
+    //String
+    let name = "petter"
+    console.log(name);
+
+    //Boolean              ==> true or false
+    let isLogin = true 
+    console.log(isLogin);
+    
+    //null   standalone vale 
+    let userName = ""
+
+// undefined not defined ar all
+
+//symbol is a unique one 
+
+
+// "33" => 33
+// "78gu6" => NaN
+// true => 1  false => 0
+
+
+
