b26e7cb7be4c6bba3ec58d60b071debe704d86e4
Ponnesh18 <ponnesh18@gmail.com>
2026-04-13 08:50:39 +0530

    first commit and adding code

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var name = "petter"
+let userName = "userPeter"
+const middleName = "peter parker"
+let number = 1234567890
+let isLogin = true
+
+//  var name = "petter spider"
+//  let userName = "userPeter spider"
+//  const middleName = "peter parker spider"
+
+console.log(name, userName, middleName, isLogin, number);
