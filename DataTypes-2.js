3c157022d4e1ee16686e40f4e8fcc26c986c3c9d
Ponnesh18 <ponnesh18@gmail.com>
2026-04-14 10:55:43 +0530

    DataTypes first commit

---
+++ DataTypes-2.js
@@ -0,0 +1,25 @@
+// Primitive   7 types :- String , Number , Boolean , null , undefined , Symbol(‘12’) ,BigInt 12n
+
+// Reference ( non primitive)  object { }, array [ ], functions ( ) { } 
+
+let name = "petter"
+console.log(name);
+
+let phNumber = 2314567589
+console.log(phNumber);
+
+let isLogged = true
+console.log(isLogged);
+
+let empty = null
+console.log(empty);
+
+let useName
+
+const id = Symbol('123');
+const anotherID = Symbol('123')
+
+
+
+
+
