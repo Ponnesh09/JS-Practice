18fc6462bf74ec33905f465fc272e2f1b6329b85
Ponnesh18 <ponnesh18@gmail.com>
2026-04-14 10:14:48 +0530

    String conversion file first commit

---
+++ stringConversion.js
@@ -0,0 +1,36 @@
+
+//Operator 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "Son"
+let str2 = " Goku"
+// let str3 = str1 - str2;   NaN
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("12" + 12);                     1212
+// console.log("5" - 2);                       3   
+// console.log("21" + "21");                   2121
+// console.log("20" - "10");                   10
+// console.log("20" * 2);                      40
+// console.log(1 +"21" + "21");                12121
+// console.log(2+"20" - "10");                 210
+// console.log((5 - 4) + 2 + 10 * 5 % 2);      3
+
+
+
+
+
