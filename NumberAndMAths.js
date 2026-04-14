77c172a1b40787d76a5404c2aa997ef789fa9027
Ponnesh18 <ponnesh18@gmail.com>
2026-04-14 13:40:25 +0530

    first commit of Number and math

---
+++ NumberAndMAths.js
@@ -0,0 +1,28 @@
+
+let score = 100
+// console.log(score);            //100
+
+let newScore = new Number(100)
+// console.log(newScore);          //[Number: 100]
+
+console.log(newScore.toString().length);      // 3
+// console.log(newScore.toFixed(1));            // 100.0
+
+let newNumber = 28.97887
+// console.log(newNumber.toPrecision(5))            // 28.979
+
+const count = 524000000000
+// console.log(count.toLocaleString('en-IN'));          //5,24,00,00,00,000
+
+//+++++++++   Math   +++++++++++++++ 
+console.log(Math);
+console.log(Math.random());
+console.log(Math.abs(-4));
+console.log(Math.round(45.689789));
+console.log(Math.ceil(25.6555));
+console.log(Math.floor(26.6565));
+console.log(Math.min(2,865,34,4,3));
+console.log(Math.max(2,865,34,4,3));
+
+
+
