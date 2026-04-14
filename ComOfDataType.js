0dbe3bd635181302166e9d15715a4610073e8d93
Ponnesh18 <ponnesh18@gmail.com>
2026-04-14 10:42:20 +0530

    Comparison of datatype in javascript

---
+++ ComOfDataType.js
@@ -0,0 +1,34 @@
+
+// console.log(2 < 5);
+// console.log(3 > 6);
+// console.log(6 >= 10);
+// console.log(5 <= 5 );
+// console.log(10 == 10);
+// console.log(8 != 2);
+// console.log("2" > 1);
+// console.log(2 > "1");
+
+// console.log(null > 0);      false
+// console.log(null >= 0);     true
+// console.log(null == 0);     false
+
+// console.log(undefined > 0);     false
+// console.log(undefined < 0 );    false
+// console.log(undefined == 0);    false
+
+// ===  !===  , ==  !==  ,  =  
+
+
+// console.log(10 === 10);   t
+// console.log(10 === "10"); f
+// console.log(10 == "10");  t
+// console.log(10 === "5");  f 
+
+
+
+
+
+
+
+
+
