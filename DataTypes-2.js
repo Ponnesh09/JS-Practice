50931d31951e5508635cf90f9939d89b27ad5824
Ponnesh18 <ponnesh18@gmail.com>
2026-04-14 11:48:58 +0530

    Data types 2

--- DataTypes-2.js
+++ DataTypes-2.js
@@ -1,25 +1,46 @@
 // Primitive   7 types :- String , Number , Boolean , null , undefined , Symbol(‘12’) ,BigInt 12n
 
-// Reference ( non primitive)  object { }, array [ ], functions ( ) { } 
+
 
 let name = "petter"
-console.log(name);
+// console.log(name);
 
 let phNumber = 2314567589
-console.log(phNumber);
+// console.log(phNumber);
 
 let isLogged = true
-console.log(isLogged);
+// console.log(isLogged);
 
 let empty = null
-console.log(empty);
+// console.log(empty);
 
 let useName
 
 const id = Symbol('123');
 const anotherID = Symbol('123')
+// console.log(id , anotherID);
+
+let BigInt = 763456738547654785n;
+// console.log(BigInt);
 
 
+// Reference ( non primitive)  object { }, array [ ], functions ( ) { } 
 
+ let myOject = {
+    name : "petter",
+    age : "21"
+ }
 
+ console.log(myOject.name);
+ 
 
+ let myArray =  [1,2,3,4,5,6]
+ console.log(myArray[3]);
+
+ let myFun = function () {
+    console.log(`hi this is function syntax`);
+    
+ }
+
+ 
+ 
