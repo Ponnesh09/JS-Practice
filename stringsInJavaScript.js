d7b95e1bd67246605200742062dfab22b70e0282
Ponnesh18 <ponnesh18@gmail.com>
2026-04-14 13:13:18 +0530

    string first commit

---
+++ stringsInJavaScript.js
@@ -0,0 +1,45 @@
+// template literals
+
+let name = "jon"
+let age = 30
+
+// console.log(`My name is ${name} and i am ${age} year old `);
+
+const gameName = new String('ponneshRamaswsamy')
+// console.log(gameName[0,6]);
+
+//finding proto
+
+// in browser 
+// const gameName = new String('ponnesh')
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLocaleUpperCase());
+
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('n'));
+
+
+const newStrings = gameName.substring(0,5);
+// console.log(newStrings);
+
+const  anotherNewString = gameName.slice(5, 10);
+// console.log(anotherNewString);
+
+
+const newStringOne = "      petter      "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+// console.log(newStringOne.trimStart());
+// console.log(newStringOne.trimEnd());
+
+
+const url = "https://www.youtube.google.com/"
+
+console.log(url.replace('.', '$'));
+
+//it check the value you given it includes on url or not
+console.log(url.includes('movie'));
