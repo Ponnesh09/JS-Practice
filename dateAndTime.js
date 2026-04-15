13fd93b7ed2d5c0ea57932a2dfe4cba4fc39c46d
Ponnesh18 <ponnesh18@gmail.com>
2026-04-15 08:36:36 +0530

    Date and Time

---
+++ dateAndTime.js
@@ -0,0 +1,30 @@
+
+let myDate = new Date()
+// console.log(myDate);
+
+myDate.toLocaleString();
+// console.log(myDate.toLocaleString());
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+
+
+let myCreatedDate = new Date(2025, 0,1 )    //day, month, date 1 to 31     it all start from 0  index value is 0
+// console.log(myCreatedDate.toLocaleDateString());
+
+console.log(myCreatedDate.getTime());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getMonth());
+
+
+
+
