5d989758da76c1376c556427b17f62a5b860ae3c
Ponnesh18 <ponnesh18@gmail.com>
2026-04-16 14:01:43 +0530

    Arrow function

--- this-arrowFun.js
+++ this-arrowFun.js
@@ -23,6 +23,25 @@ const chai = function () {
 chai()
 
 
+//Arrow Function
+
+// Explicit with return
+let sum = (num1, num2) => {return num1 + num2}
+console.log(sum(5,5));
+
+//Implicit without return     
+let sum1 = (num1, num2) =>  num1 + num2
+console.log(sum1(5,5));
 
 
 
+
+//object in arrow function
+
+// () => ({userName: "ponnesh"})
+
+// const array1 = [1,2,3,4,5]
+
+// array1.forEach(function(){})
+// array1.forEach(()=>{})
+// array1.forEach(()=>(user))
