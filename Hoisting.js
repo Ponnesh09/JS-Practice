9c1774a9c180e2de0ba30af148321f99eca43731
Ponnesh18 <ponnesh18@gmail.com>
2026-04-16 13:28:07 +0530

    Hoisting first commit

---
+++ Hoisting.js
@@ -0,0 +1,32 @@
+
+function outer () {
+    let user = "petter"
+
+    function inner () {
+        let role = "developer"
+        // console.log(`i am ${user} and my role is ${role}`);
+    }
+    inner()
+
+    // console.log(user);
+    // console.log(role);
+        
+}
+
+outer()
+
+
+
+if(true){
+    const animal = "Dog"
+
+    if(animal === "Dog"){
+        const sound = "barking"
+        // console.log(`your ${animal} is ${sound}`);
+    }
+}
+
+
+
+
+
