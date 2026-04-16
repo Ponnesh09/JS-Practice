2792b9a20d2e369254d4bfeb9edd6965026b85b5
Ponnesh18 <ponnesh18@gmail.com>
2026-04-16 13:54:25 +0530

    this complete

---
+++ this-arrowFun.js
@@ -0,0 +1,28 @@
+const user ={
+    user : "jon",
+    price : 200 ,
+
+    welcomeMessage : function () {
+            console.log(`${this.user}  welcome to our website`);
+            
+    }
+}
+
+// user.welcomeMessage()
+
+user.user = 'sam'
+// user.welcomeMessage()
+
+//console.log(this);        //out put {}
+
+
+const chai = function () {
+    let userName = "petter chai"
+    // console.log(userName);
+}
+chai()
+
+
+
+
+
