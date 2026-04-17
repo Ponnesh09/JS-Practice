e2054a70e382e81e815059dd42cf6429cead6a4c
Ponnesh18 <ponnesh18@gmail.com>
2026-04-17 12:17:03 +0530

    for loop first commit

---
+++ forLoop.js
@@ -0,0 +1,89 @@
+//for loop
+
+// syntax
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];  
+// }
+
+// Iteration  ===  loops
+
+for(let i=1 ; i <= 10 ; i++){
+    let result = i;
+    // console.log(result);
+}
+
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     let result1 = i;
+//     if(result1 == 5){console.log("5 is the best part");
+//     }
+//     // console.log(result1);
+    
+// }
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log(`50 message`);
+    
+// }
+
+
+
+for(let p = 1 ; p <= 10 ; p++){
+    // console.log(`${p} outer for loop`);
+        for(let m = 1 ; m <= 10 ; m++){
+            // console.log(`${m} inner loop`);
+            // console.log(p + '*' + m + "=" + p*m);         table 
+            
+        }    
+}
+
+
+//looping array
+
+const myArray = ["red", "yellow", "purple", "black", "orange", "pink", "white"] 
+
+// for(let i = 0; i < myArray.length ; i++){
+//     const element = myArray[i];
+//     console.log(element);
+    
+// }
+
+
+// for(let  i = 0 ; i < myArray.length ; i++){
+//     const element = myArray[i]
+//     console.log(`color index ${i} ${element}`);
+    
+// }
+
+
+// for loop with Break 
+
+// for(let i = 0; i<= 20 ; i++){
+//     if(i == 5){
+//         console.log("reached 5 break alert");
+//         break;
+//     }
+//     console.log(`🥰🥰🥰value of i ${i}`);
+// }
+
+
+//  for loop with continue
+
+// for(let i = 0 ; i < 10 ; i++){
+//     if(i == 7){
+//         console.log("we reached 7  continue alert");
+//         continue
+//     }
+//     console.log(`🤩🤩🤩🤩value of i ${i}`);
+    
+// }
+
+
+// for (let i = 0; i < 5; i++) {
+//     if (i === 3) continue; // Skips the rest of the loop when i is 3
+//     console.log(i);
+// }
+
+
+
+
