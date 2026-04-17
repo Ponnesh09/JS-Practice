a49d1a681e7adad05b4a6896c7b87af1f5d20f5c
Ponnesh18 <ponnesh18@gmail.com>
2026-04-17 12:48:01 +0530

    while and do while

---
+++ while-doWhile-loop.js
@@ -0,0 +1,56 @@
+
+// while loop syntax
+
+// while (condition) {
+    
+// }
+
+
+let g = 0
+while (g <=10){
+    // console.log("i am from while loop , value of index " + g);
+    g++
+}
+
+
+let R = 0
+while (R <= 10){
+    // console.log(`i am 2nd while loop, value of index R ${R}`);
+    R += 2
+    // R = R + 2
+}
+
+
+//while loop in array
+
+const myArray = ["red", "yellow", "purple", "black", "orange", "pink", "white"] 
+
+let arr = 0;
+while(arr < myArray.length){
+    // console.log(`value of index ${arr}, ${myArray[arr]}`);
+    // arr +=1
+    // arr++
+    // arr = arr + 3  odd value
+    // arr = arr + 2  even number
+}
+
+
+//do while loop syntax
+
+// do {
+    
+// } while (condition);
+
+// let dw = 11
+// do{
+//     console.log(`value of index ${dw}`);
+//     dw++
+    
+// }while(dw <= 20);
+
+
+// let i = 0;
+// do {
+//   console.log("Iteration:", i);
+//   i++;
+// } while (i < 5);
