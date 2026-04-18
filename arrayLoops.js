b3eb250d40adde633a0572c2ea4b4d7207461a15
Ponnesh18 <ponnesh18@gmail.com>
2026-04-18 07:53:18 +0530

    for of loop commit

---
+++ arrayLoops.js
@@ -0,0 +1,31 @@
+// high Order array loop 
+
+
+// data format 
+// ["", "",""]
+// [{},{},{}]
+
+//We fetch value using   .notation, []notation
+
+// syntax of for of loop
+// for (const element of object) {
+    
+// }
+
+
+const arr = ["one","two","three","four","five"]
+
+for(const num of arr){
+    // console.log(num);
+    
+}
+
+
+let greeting = "hii good morning"
+
+for(const greet of greeting){
+    // console.log(`each character is ${greet}`);
+      
+}
+
+// map 
