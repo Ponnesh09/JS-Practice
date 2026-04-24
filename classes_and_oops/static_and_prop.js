d73e189e5726fe5385709382a85664bd18789d2a
Ponnesh18 <ponnesh18@gmail.com>
2026-04-24 11:56:47 +0530

    Static

--- classes_and_oops/static_and_prop.js
+++ classes_and_oops/static_and_prop.js
@@ -25,12 +25,12 @@ class Teach extends User {
 
 const newUser = new Teach("jon" ,"email.com",9870)
 // console.log(newUser);
-newUser.addLecture()
+// newUser.addLecture()
 
 
 
 const newUserMore = new User("new joint")
-newUserMore.logMe()
+// newUserMore.logMe()
 
 
 
@@ -39,4 +39,42 @@ newUserMore.logMe()
 
 
 
-  
+// Statics and prop >>> properties 
+
+
+class UserOne {
+    constructor(userName){
+            this.userName = userName;
+    }
+
+    logMe(){
+        console.log(`User Name ${this.userName}`);
+        
+    }
+
+    // createId(){
+    //     return `231`
+    // }
+    
+     static createId(){
+        return `231`
+    }
+}
+
+const petter = new UserOne("petter")
+// console.log(petter);
+
+// console.log(petter.createId());
+
+
+class Teacher extends UserOne {
+    constructor(userName , email) {
+        super(userName);
+        this.email =email
+    }
+}
+
+const sam = new Teacher("petter","petter@gmail.com")
+console.log(sam);
+
+// console.log(sam.createId());  error
