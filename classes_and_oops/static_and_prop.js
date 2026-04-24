b7caa1eacfef9819bf2ba4ff35a05d37fa5fff73
Ponnesh18 <ponnesh18@gmail.com>
2026-04-24 11:33:56 +0530

    Static

---
+++ classes_and_oops/static_and_prop.js
@@ -0,0 +1,33 @@
+class User {
+    constructor(userName) {
+        this.userName = userName;
+    }
+
+    logMe(){
+        console.log(`User Name is ${this.userName} `);
+        
+    }
+}
+
+
+class Teach extends User {
+    constructor(userName,email,passWord){
+            super(userName)
+            this.email = email;
+            this.passWord = passWord
+    }
+
+    addLecture(){
+        console.log(`a new course added by ${this.userName}`);
+    }
+}
+
+
+const newUser = new Teach("jon" ,"email.com",9870)
+// console.log(newUser);
+newUser.addLecture()
+
+
+
+const newUserMore = new User("new joint")
+newUserMore.logMe()
