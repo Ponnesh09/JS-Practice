8bec4adf15a5be11f7ce2a02dda48084585a2050
Ponnesh18 <ponnesh18@gmail.com>
2026-04-24 19:12:31 +0530

    Object 2nd Part Done

---
+++ classes_and_oops/Object_Two.js
@@ -0,0 +1,58 @@
+// mathpi.js
+
+// Object.getOwnPropertyDescriptor(Math)
+
+console.log(Math.PI);
+
+// Math.PI = 5;
+// console.log(Math.PI); //it can not change
+
+
+const descriptor = Object.getOwnPropertyDescriptor(Math,"PI")
+// console.log(descriptor);
+
+//output
+
+// {
+//     value: 3.141592653589793,
+//     writable: false,
+//     enumerable: false,
+//     configurable: false
+//   }
+
+const book = {
+    title : "Avenger",
+    edition : "Comic Book",
+    price : 100,
+    bookOrder : function (){
+        console.log("book sold out");
+        
+    }
+
+}
+
+// console.log(Object.getOwnPropertyDescriptor(book, "title"));   r,r,r
+
+// console.log(Object.getOwnPropertyDescriptor(book.title)); ❌❌❌❌
+
+
+
+Object.defineProperty(book,"title",{
+    writable: false,
+    enumerable: true,
+    configurable: true,
+    iterable: false,
+    
+})
+
+// console.log(Object.getOwnPropertyDescriptor(book, "title"));   false,true,true
+
+
+for(let[key,value] of Object.entries(book)){
+    if(typeof value !== 'function'){
+        console.log(`${key}:${value}`);
+        
+    }
+}
+
+
