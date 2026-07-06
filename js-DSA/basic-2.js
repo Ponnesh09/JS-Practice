9672085b782b232d3723b6c50dd4b674ab4b35e0
Ponnesh18 <ponnesh18@gmail.com>
2026-07-06 13:58:18 +0530

    done Q1 print number of *

--- js-DSA/basic-2.js
+++ js-DSA/basic-2.js
@@ -5,7 +5,15 @@ let prompt = require('prompt-sync')();
 // process.stdout.write("world")
 
 
-let pt = prompt("enter number")
+// let pt = prompt("enter number")
 // process.stdout.write(pt)
 
+
+// Q1 print number of *
+
+let num = Number(prompt("Enter number"))
+
+for(let i= 1 ; i<=num; i++){
+    process.stdout.write("* ");
+}
 
