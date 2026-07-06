cf671868afeed197869464e79d2ab87a06b54ddc
Ponnesh18 <ponnesh18@gmail.com>
2026-07-06 13:53:02 +0530

    set up

---
+++ js-DSA/basic-2.js
@@ -0,0 +1,11 @@
+let prompt = require('prompt-sync')();
+
+
+// process.stdout.write("hello ")
+// process.stdout.write("world")
+
+
+let pt = prompt("enter number")
+// process.stdout.write(pt)
+
+
