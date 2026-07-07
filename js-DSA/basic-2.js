f9457b49c6375b0749cbaa93e2ade16a2b7597fd
Ponnesh18 <ponnesh18@gmail.com>
2026-07-07 20:40:11 +0530

    done mirror right angle triangle

--- js-DSA/basic-2.js
+++ js-DSA/basic-2.js
@@ -90,13 +90,44 @@ let prompt = require('prompt-sync')();
 
 // Q7  inverted right angle triangle
 
- let n = Number(prompt("enter number "))
+//  let n = Number(prompt("enter number "))
 
-for(let i=1; i<=n; i++){
-        for(let j=1; j<=n-i+1;j++){
+// for(let i=1; i<=n; i++){
+//         for(let j=1; j<=n-i+1;j++){
+//         process.stdout.write("* ");
+
+//         }
+//         console.log(" ");
+        
+// }
+
+// Q8 mirror right angle triangle
+let n = Number(prompt("enter "))
+
+
+for(let i =1; i<=n; i++){
+
+    //invert
+    for(let j=1; j<=n-i; j++){
+        process.stdout.write("  ");
+    }
+
+    // right angle
+    for(let j=1; j<=i; j++){
         process.stdout.write("* ");
 
-        }
-        console.log(" ");
-        
+    }
+console.log(" ");
+
 }
+
+
+
+
+
+
+
+
+
+
+
