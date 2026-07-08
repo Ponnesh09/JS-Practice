cb246354ca4417bf00d3691dfc6482ea1b0f65b1
Ponnesh18 <ponnesh18@gmail.com>
2026-07-08 14:39:27 +0530

    done revers Array

---
+++ js-DSA/reversArray.js
@@ -0,0 +1,20 @@
+// Q9 reverse the array
+
+let arr = [10,20,30,40,50]
+let i = 0 , j= arr.length-1;
+
+while(i != j){
+let temp = arr[i]
+arr[i] = arr[j]
+arr[j] = temp
+i++
+j--
+}
+
+console.log(arr);
+
+
+
+
+
+
