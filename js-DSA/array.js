542f16f186b0e4162fd24f5503b63c85e0b52628
Ponnesh18 <ponnesh18@gmail.com>
2026-07-08 12:33:46 +0530

    array using [ ]

---
+++ js-DSA/array.js
@@ -0,0 +1,18 @@
+// 1at method
+
+let arr = [1,2,3,4]
+
+arr.push(5) //add from last
+arr.pop()  // remove from last
+arr.push("pushing")
+arr.push(true)
+arr.push(10)
+
+
+console.log(arr);
+console.log(arr[2]);
+
+
+
+
+
