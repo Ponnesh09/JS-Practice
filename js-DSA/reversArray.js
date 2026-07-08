439dc5f68ad99bc0c1651e1ea48449a5925e5af7
Ponnesh18 <ponnesh18@gmail.com>
2026-07-08 14:44:48 +0530

    Interview Tip

--- js-DSA/reversArray.js
+++ js-DSA/reversArray.js
@@ -1,20 +1,34 @@
 // Q9 reverse the array
 
 let arr = [10,20,30,40,50]
-let i = 0 , j= arr.length-1;
+// let i = 0 , j= arr.length-1;
 
-while(i != j){
-let temp = arr[i]
-arr[i] = arr[j]
-arr[j] = temp
-i++
-j--
+// while(i != j){
+// let temp = arr[i]
+// arr[i] = arr[j]
+// arr[j] = temp
+// i++
+// j--
+// }
+
+// console.log(arr);
+
+
+
+
+// Interview Tip
+
+// To reverse an array using two pointers:
+
+let left = 0;
+let right = arr.length - 1;
+
+while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
 }
-
 console.log(arr);
 
-
-
-
-
+// This is the standard in-place reversal algorithm with O(n) time complexity and O(1) extra space.
 
